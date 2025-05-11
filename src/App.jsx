3be25bcf8f68039f912fb1d8137bb3e3410eda1a
Prit-am1 <roychowdhurypritam372@gmail.com>
2025-05-12 00:18:56 +0530

    Learned about React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
+import { createBrowserRouter } from 'react-router-dom'
+import About from './components/About'
+import Contact from './components/Contact'
+import Error from './components/Error'
+
 
 function App() {
 
@@ -12,4 +16,21 @@ function App() {
   )
 }
 
-export default App
+const appRouter =
+  createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <Error />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    }
+  ])
+
+export default appRouter
